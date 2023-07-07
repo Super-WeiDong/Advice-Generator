1d2163ec0d3429cefdc358df96b91041287c6315
Super-WeiDong <weidongwang@brandeis.edu>
2023-07-07 10:45:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function app() {
+  const [advice, setAdvice] = new useState("");
+  const [count, setCount] = new useState(0);
+  const [time, setTime] = new useState(new Date().toLocaleTimeString());
+  new useEffect(function () {
+    getAdvice();
+  }, []);
+
+  new useEffect(function () {
+    setInterval(function () {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount(count + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> {advice} </h1>
+      <button onClick={getAdvice}> Get Advice </button>
+      <Message count={count} />
+      <TimeBoard time={time} />
     </div>
   );
 }
 
-export default App;
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advices{" "}
+    </p>
+  );
+}
+
+function TimeBoard(props) {
+  return (
+    <p>
+      Current Time is: <strong>{props.time}</strong>
+    </p>
+  );
+}
